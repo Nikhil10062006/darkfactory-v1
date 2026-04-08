25d284c3a7f02ca2416ac0429127e675ea90fbc6
Swarit1212 <swaritagrawalrrkl@gmail.com>
2026-04-08 20:13:04 +0530

    commit 3

--- server/app.py
+++ server/app.py
@@ -2,10 +2,10 @@ import uvicorn
 import os
 from server.main import app
 
-def start():
+def main():
     port = int(os.environ.get("PORT", 7860))
     host = os.environ.get("HOST", "0.0.0.0")
     uvicorn.run("server.main:app", host=host, port=port)
 
 if __name__ == "__main__":
-    start()
+    main()
