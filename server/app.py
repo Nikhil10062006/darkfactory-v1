22cd4d83b83bb3b8f69cd5484c545bdec67120d8
Swarit1212 <swaritagrawalrrkl@gmail.com>
2026-04-08 20:08:04 +0530

    2nd commit

---
+++ server/app.py
@@ -0,0 +1,11 @@
+import uvicorn
+import os
+from server.main import app
+
+def start():
+    port = int(os.environ.get("PORT", 7860))
+    host = os.environ.get("HOST", "0.0.0.0")
+    uvicorn.run("server.main:app", host=host, port=port)
+
+if __name__ == "__main__":
+    start()
